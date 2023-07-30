7f65b601e3a92598b36a85c20a6fe357d79dd926
George Wan <george.wan@uwaterloo.ca>
2023-07-30 01:13:10 -0400

    Change board test to filestream

--- tests/board/board_tests.cc
+++ tests/board/board_tests.cc
@@ -3,7 +3,7 @@
 #include "../../src/board/tile.h"
 #include "../../src/board/vertex.h"
 #include "gtest/gtest.h"
-#include <iostream>
+#include <fstream>
 
 std::vector<TileInitData> sampleTileInitData = {{3, BRICK}, {10, ENERGY}, {5, HEAT},  {4, ENERGY}, {7, PARK},   {10, HEAT}, {11, GLASS},
                                                 {3, BRICK}, {8, HEAT},    {2, BRICK}, {6, BRICK},  {8, ENERGY}, {12, WIFI}, {5, ENERGY},
@@ -22,7 +22,8 @@ TEST(Board, InitBoardCorrectQuantities) {
         EXPECT_EQ(i, board.getVertex(i)->getVertexNumber());
     }
 
-    board.printBoard(std::cout);
+    std::ofstream file("board.out");
+    board.printBoard(file);
 }
 
 // TODO: Write tests to verify that tileValues and resources are set correctly
