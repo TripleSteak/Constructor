00821aa5f5cdbda7117ae361428eec58af193429
George Wan <george.wan@uwaterloo.ca>
2023-07-30 23:58:22 -0400

    Add builder overload constructor

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 cd "${0%/*}"
 
+if [ -f "tests.out" ]; then
+    rm tests.out
+fi
+
 echo "Compiling tests..."
 # g++ -g -std=c++17 -Wall ../src/*/*.cc -pthread */*.cc -lgtest -lgtest_main -o tests.out
 make
 
 echo "Running tests..."
 ./tests.out
-# rm tests.out
